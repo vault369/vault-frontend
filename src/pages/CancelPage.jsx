bcf64be8837c65f0292ea0b79befe77c74315dbb
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-05 15:40:24 -0600

    Create CancelPage.jsx

---
+++ src/pages/CancelPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CancelPage = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>❌ Mint Cancelled</h1>
+      <p>No worries! You can try minting again anytime.</p>
+    </div>
+  );
+};
+
+export default CancelPage;
