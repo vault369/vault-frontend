bf8de07934685552c7de812efc2ea3c00e72cab5
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-04 22:22:54 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import TrackList from './pages/TrackList';
 import TrackDetail from './pages/TrackDetail';
@@ -7,13 +8,22 @@ import ArtistDashboard from './pages/ArtistDashboard';
 
 function App() {
   return (
-    <>
-      <HomePage />
-      <TrackList />
-      <TrackDetail />
-      <UploadPage />
-      <ArtistDashboard />
-    </>
+    <div className="text-white font-mono">
+      <nav className="bg-black border-b border-gray-800 px-6 py-4 flex justify-center gap-6 text-cyan-300">
+        <Link to="/">Home</Link>
+        <Link to="/tracks">Tracks</Link>
+        <Link to="/detail">Track Detail</Link>
+        <Link to="/upload">Upload</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/tracks" element={<TrackList />} />
+        <Route path="/detail" element={<TrackDetail />} />
+        <Route path="/upload" element={<UploadPage />} />
+        <Route path="/dashboard" element={<ArtistDashboard />} />
+      </Routes>
+    </div>
   );
 }
 
