8ac2463bca73e74949fd38244b6a8f558ba2960c
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-05 15:39:46 -0600

    Create SuccessPage.jsx

---
+++ src/pages/SuccessPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SuccessPage = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>✅ Mint Successful</h1>
+      <p>Thank you! Your track has been minted.</p>
+    </div>
+  );
+};
+
+export default SuccessPage;
