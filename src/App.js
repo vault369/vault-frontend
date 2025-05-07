77722cb94f4294f5f31b9741e11c1552e5d6ed0f
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-07 04:07:46 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import MintPage from "./pages/MintPage";
 import TrackPage from "./pages/TrackPage";
 import SuccessPage from "./pages/SuccessPage";
 import CancelPage from "./pages/CancelPage";
+import LoginPage from "./pages/LoginPage";
+import UploadPage from "./pages/UploadPage";
 
 function App() {
   return (
     <Router>
       <div style={{ padding: "1rem", borderBottom: "1px solid #333" }}>
         <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
-        <Link to="/mint">Mint</Link>
+        <Link to="/mint" style={{ marginRight: "1rem" }}>Mint</Link>
+        <Link to="/login" style={{ marginRight: "1rem" }}>Login</Link>
+        <Link to="/upload">Upload</Link>
       </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
@@ -19,6 +23,8 @@ function App() {
         <Route path="/track/:id" element={<TrackPage />} />
         <Route path="/success" element={<SuccessPage />} />
         <Route path="/cancel" element={<CancelPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/upload" element={<UploadPage />} />
       </Routes>
     </Router>
   );
