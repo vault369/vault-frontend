0f7baf3fae92ddc93a5725a83bdcc39a2447cda8
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-10 23:13:53 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import MintPage from "./pages/MintPage";
-import TrackPage from "./pages/TrackPage";
-import SuccessPage from "./pages/SuccessPage";
-import CancelPage from "./pages/CancelPage";
-import LoginPage from "./pages/LoginPage";
 import UploadPage from "./pages/UploadPage";
+import TrackPage from "./pages/TrackPage";
 import UnlockPage from "./pages/UnlockPage";
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: "1rem", borderBottom: "1px solid #333" }}>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/mint">Mint</Link> |{" "}
-        <Link to="/login">Login</Link> |{" "}
-        <Link to="/upload">Upload</Link>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/upload">Upload</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/mint" element={<MintPage />} />
-        <Route path="/track/:id" element={<TrackPage />} />
-        <Route path="/success" element={<SuccessPage />} />
-        <Route path="/cancel" element={<CancelPage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/upload" element={<UploadPage />} />
+        <Route path="/track/:id" element={<TrackPage />} />
         <Route path="/unlock/:id" element={<UnlockPage />} />
       </Routes>
     </Router>
