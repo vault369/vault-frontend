cc968c3ea68855f9f038f2d73e3137847c69b74d
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-11 00:37:51 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import HomePage from "./pages/HomePage";
 import UploadPage from "./pages/UploadPage";
 import TrackPage from "./pages/TrackPage";
 import UnlockPage from "./pages/UnlockPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/upload">Upload</Link>
+        <Link to="/">Home</Link> | <Link to="/upload">Upload</Link> | <Link to="/login">Login</Link>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/track/:id" element={<TrackPage />} />
         <Route path="/unlock/:id" element={<UnlockPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
