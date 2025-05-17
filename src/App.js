14aca95343340d9f0543383bacec3cbc486181c8
vault369 <vaultdigitalpressings@protonmail.com>
2025-05-17 12:07:19 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import GenrePage from './pages/GenrePage';
 import UploadPage from './pages/UploadPage';
-import TrackPage from './pages/TrackPage';
-import UnlockPage from './pages/UnlockPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import TrackPage from './pages/TrackPage';
+import UnlockPage from './pages/UnlockPage';
 
-const App = () => {
+function App() {
+  const token = localStorage.getItem('vaultToken');
+
   return (
     <Router>
       <nav className="container">
-        <Link to="/">Home</Link> | <Link to="/upload">Upload</Link> | <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+        <Link to="/">Home</Link> | 
+        {token && <Link to="/upload">Upload</Link>} | 
+        {token 
+          ? <span style={{ color: 'lime' }}>Logged in</span> 
+          : <Link to="/login">Login</Link>} | 
+        {!token && <Link to="/register">Register</Link>}
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
-        <Route path="/track/:id" element={<TrackPage />} />
-        <Route path="/unlock/:id" element={<UnlockPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/track/:trackId" element={<TrackPage />} />
+        <Route path="/unlock/:trackId" element={<UnlockPage />} />
+        <Route path="/genre/:genreName" element={<GenrePage />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
